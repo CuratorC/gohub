9b57efa20ffd3ef1c8c9a5a50d0ca62279ff83d7
CuratorC <mail@curatorc.com>
2022-01-12 11:12:27 +0800

    response 包

--- app/http/middlewares/recovery.go
+++ app/http/middlewares/recovery.go
@@ -2,8 +2,8 @@ package middlewares
 
 import (
 	"gohub/pkg/logger"
+	"gohub/pkg/response"
 	"net"
-	"net/http"
 	"net/http/httputil"
 	"os"
 	"strings"
@@ -40,7 +40,10 @@ func Recovery() gin.HandlerFunc {
 						zap.Any("error", err),
 						zap.String("request", string(httpRequest)),
 					)
-					c.Error(err.(error))
+					err := c.Error(err.(error))
+					if err != nil {
+						return
+					}
 					c.Abort()
 					// 链接已断开，无法写状态码
 					return
@@ -55,9 +58,7 @@ func Recovery() gin.HandlerFunc {
 				)
 
 				// 返回 500 状态码
-				c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
-					"message": "服务器内部错误，请稍后再试",
-				})
+				response.Abort500(c)
 			}
 		}()
 		c.Next()
